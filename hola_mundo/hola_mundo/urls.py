9a1772460ac1d5759364c6a8999f93f75cb5466a
Halia Litai Lopez Alvarez <123999262+litailopez@users.noreply.github.com>
2024-06-02 21:07:25 -0600

    Delete hola_mundo directory

--- hola_mundo/hola_mundo/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from mensaje import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('hola/', views.mensaje),
-    path('nombre/', views.funcion_nombre),
-]
